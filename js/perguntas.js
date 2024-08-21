8cbc87d567847a25e7f99ecba4eae75f289d25a1
izabellycl <117642132+izabellycl@users.noreply.github.com>
2024-08-21 12:18:55 -0300

    .

---
+++ js/perguntas.js
@@ -0,0 +1,63 @@
+const perguntas = [
+    {
+        enunciado: "você concorda que racismo é crime?",
+        alternativas: [
+            {
+                texto: "sim,concordo",
+                afirmacao: [
+                    "racismo pode levar a suicidio, depresão, má convivência com familiares, baixa estima, entre outras consequências que não leva a pessoa para frente e sim a ter pensamentos ruins. 
+",
+                    "afirmacao 2"
+                    ]
+            },
+            {
+                texto: "não, não concordo",
+                afirmacao: [
+                    "vocẽ não está certo. racismo é crime por ser um desrespeito a vítima.
+",
+                    "afirmacao 2"
+                    ]
+            }           
+            
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            {
+                texto:"Alternativa 1 da pergunta 2",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            },
+            {
+                texto: "Alternativa 2 da pergunta 2",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            }
+        ]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: [
+            {
+                texto:"Alternativa 1 da pergunta 3",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            },
+            {
+                texto:"Alternativa 2 da pergunta 3",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            }
+            
+        ]
+    },
+];
